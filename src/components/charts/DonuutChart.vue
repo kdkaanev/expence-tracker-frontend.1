646685298b5bd330d7d635b9f48c7219b909e873
Kancho Kanev <Kancho Kanev@bulmetal.com>
2025-09-11 11:43:01 +0300

    dasboard style

--- src/components/charts/DonuutChart.vue
+++ src/components/charts/DonuutChart.vue
@@ -1,11 +1,29 @@
 <script setup>
+  import { Chart as ChartJS, ArcElement, Tooltip, Legend, } from 'chart.js';
+  import { Doughnut} from 'vue-chartjs';
+
+
+  ChartJS.register(ArcElement, Tooltip, Legend,);
+
+  defineProps({
+  chartData: {
+    type: Object,
+    required: true,
+  },
+  chartOptions: {
+    type: Object,
+    required: true,
+  },
+  });
+
 
 </script>
 
 <template>
-<h1 class="text-3xl font-bold underline">
-  DonutChart
-</h1>
+  <div class="chart-container">
+    <Doughnut :data="chartData" :options="chartOptions" />  
+
+  </div>
 </template>
 
 <style scoped>
--- src/pages/DashBoard.vue
+++ src/pages/DashBoard.vue
@@ -2,23 +2,100 @@
     import { useAuthStore } from '../store/authStore';
     import NavBar from './NavBar.vue';
     const authStore = useAuthStore();
+    import DonuutChart from '../components/charts/DonuutChart.vue';
+import { Doughnut } from 'vue-chartjs';
+   
+    const props = defineProps({
+    categories: {
+      type: Array,
+      default: () => ['Savings', 'Expenses', 'Investments'],
+    },
+    values: {
+      type: Array,
+      default: () => [300, 500, 200],
+    },
+  });
+  const chartData = {
+    labels: props.categories,
+    datasets: [
+      {
+        data: props.values,
+        backgroundColor: ['#27ae60', '#f39c12', 'e74c3c'],
+        borderWidth: 2,
+      },
+    ],
+  };
 
+  const chartOptions = {
+    responsive: true,
+    maintainAspectRatio: false,
+    cutout: '60%',
+    plugins: {
+      legend: {
+        position: 'bottom',
+        labels: {
+          color: '#333',
+          font: { size: 14 },
+
+      },
+    }
+    },
+  };
 </script>
 
 <template>
     
        
-        <section class="dashboard">
+        <article class="dashboard">
+             
+           <section class="title">
+            <h1>Welcome, Ivan!</h1>
+            <p>your financial review for September.</p>
+           </section>
+           <div class="info">
+            <section class="card">
+                <h2>Total Balance</h2>
+                <p>$5,000.00</p>
+            </section>
+            <section class="card">
+                <h2>Income this month</h2>
+                <p>$3,000.00</p>
+            </section>
+            <section class="card">
+                <h2>Expenses this month</h2>
+                <p>$1,500.00</p>
+            </section>
+            <section class="card">
+                <h2>Saving</h2>
+                <p>$1,500.00</p>
+            </section>
            
-            <h1>Welcome, {{ authStore.user?.email }}</h1>
-            <div class="card">
-                <h2>Your Profile</h2>
-                <ul>
-                    <li><strong>Email:</strong> {{ authStore.user?.email }}</li>
-                    <li><strong>User ID:</strong> {{ authStore.user?.id }}</li>
-                </ul>
-            </div>
-        </section>
+           </div>
+           <section class="charts">
+            <div style="width: 300px; height: 400px;" >
+                  <DonuutChart
+                    :chart-data="chartData"
+                    :chart-options="chartOptions"
+                />
+              </div>
+             
+            
+            <section class="card">
+                <h2>Balance for September</h2>
+                <p>$500.00</p>
+            </section>
+           </section>
+           <section class="last">
+            <h2>Last Transactions</h2>
+            <ul>
+                <li>Grocery - $50.00</li>
+                <li>Salary - $3,000.00</li>
+                <li>Electricity Bill - $100.00</li>
+                
+            </ul>
+           </section>
+           
+        </article>
     
 </template>
 
@@ -31,9 +108,47 @@
     border: 1px;
     border-radius: 8px;
     padding: 1.5rem;
-    margin-top: 1rem;
+    padding-left: 0.5rem;
+    margin-top: o.5rem;
     box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 }
+.info {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+    gap: 1rem;
+    margin-top: 1rem;
 
+}
+.card > h2{
+    margin-bottom: 0.5rem;
+    font-size: smaller;
+    font-weight: light;
+    
+}
+.card > p{
+    font-size: larger;
+    font-weight: bold;
+}
+.card > h2, .card > p {
+   text-align: start;
+  
+   
+}   
+.card:hover {
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+    transform: translateY(-2px);
+    transition: all 0.3s ease;
+}
+.dount {
+    max-width: 400px;
+    height: 400px;
+    margin: 0 auto;
+}
+.charts {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 2rem;
+    margin-top: 2rem;
+}
 </style>
 
